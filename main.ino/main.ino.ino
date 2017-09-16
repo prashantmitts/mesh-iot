770a319735c5c2dba48d1e5afbd36b1f6180c92b
prashantmitts <prashantmitts@yahoo.com>
2017-09-16 23:13:31 +0530

    added painless mesh init

---
+++ main.ino/main.ino.ino
@@ -0,0 +1,15 @@
+#include <painlessMesh.h>
+#include <painlessMeshSync.h>
+#include <painlessScheduler.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
